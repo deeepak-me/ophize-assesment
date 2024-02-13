dd43d6f0cd7ff18d9623d83edb9e9637bfa7efa8
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-13 17:04:24 +0530

    feat: server file created and connected with mongoose

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/User");
+
+mongoose.connect("mongodb://127.0.0.1:27017/auth");
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/user", userRoutes);
+
+app.listen(3000, () => {
+  console.log("Serving on port 3000");
+});
