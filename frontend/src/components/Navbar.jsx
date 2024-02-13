12553528b5c2d012584991803cb7fd1ab7c3a2ef
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-13 17:00:31 +0530

    feat: Navbar created

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+// import { useAuthContext } from "../context/authContext";
+
+const Navbar = () => {
+  //   const { user } = useAuthContext();
+
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Auth</h1>
+        </Link>
+        <nav>
+          {/* {!user && ( */}
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+          {/* )} */}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
